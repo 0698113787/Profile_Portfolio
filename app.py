549858f889c1fb13501f8dda38ec8cfa0192442e
Andile Mgazi <vuyiswaandile176@gmail.com>
2025-10-01 03:18:01 +0200

    email bug fixing it

--- app.py
+++ app.py
@@ -16,8 +16,16 @@ app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
 app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USERNAME'] = os.getenv('EMAIL_USER')
 app.config['MAIL_PASSWORD'] = os.getenv('EMAIL_PASSWORD')
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+
+# Try SSL if port is 465, otherwise use TLS
+mail_port = int(os.getenv('MAIL_PORT', 587))
+if mail_port == 465:
+    app.config['MAIL_USE_SSL'] = True
+    app.config['MAIL_USE_TLS'] = False
+else:
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
+
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('EMAIL_USER')
 
 # Increase mail timeout
